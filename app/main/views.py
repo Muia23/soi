dba703ebb0a0458f877abbc93dbc2add94613528
Muia23 <wilfred.muema@gmail.com>
2020-07-25 11:25:39 +0300

    add view a page function for blog creation form

--- app/main/views.py
+++ app/main/views.py
@@ -12,12 +12,18 @@ def index():
     
     return render_template('index.html', title = title)
 
-@main.route('/create-blog')
+@main.route('/create-blog', methods = ['GET','POST'])
 def new_blog():
     blog_form = BlogForm()
 
     if blog_form.validate_on_submit():
-        title = pitch_form.title.data
-        content = pitch_form.content.data
+        title = blog_form.title.data
+        content = blog_form.content.data
 
         new_blog = Blog(id = id, title = title, content = content)
+
+        new_blog.save_blog()
+        return redirect(url_for('main.index'))
+    
+    title = 'New Blog'
+    return render_template('new_blog.html', title = title, blog_form = blog_form)
