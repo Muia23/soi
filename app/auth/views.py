f85fbfc813b32baee1e0e5ea348d1d52d8fc6d32
Muia23 <wilfred.muema@gmail.com>
2020-07-25 18:44:08 +0300

    add a logout route, for user to log out of their account

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,6 +19,13 @@ def login():
     title = "Soi login"
     return render_template('auth/login.html', login_form = login_form, title=title)
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+    
+
 @auth.route('/register',methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
