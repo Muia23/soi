770787a2fa5b59a82613ac5fb34234b02f080a7a
Muia23 <wilfred.muema@gmail.com>
2020-07-25 10:32:27 +0300

    create a blog class

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Blog:
+    
+    def __init__(self,id,title,content):
+        
+        blog_list = []
+        
+        self.id = id
+        self.title = title
+        self.content = content
