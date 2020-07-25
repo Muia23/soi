8010cdeabcbc20040528d7df1687080c8f6365be
Muia23 <wilfred.muema@gmail.com>
2020-07-25 12:30:51 +0300

    import sqlalchemy and create a db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     
@@ -15,6 +16,7 @@ def create_app(config_name):
 
     #Initialize flask extension
     bootstrap.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)    
