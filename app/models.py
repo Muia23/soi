f862545435f8283f59ca5582d311dbd494f60926
Muia23 <wilfred.muema@gmail.com>
2020-07-25 14:07:24 +0300

    add a password column for authentification

--- app/models.py
+++ app/models.py
@@ -27,9 +27,11 @@ class Blog:
 
 class User(db.Model):
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer, primary_key =True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    
 
     def __repr__(self):
         return f'User {self.username}'
