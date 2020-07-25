26fc77d68c46caca64c0bb6f11b3b62a04fa00ba
Muia23 <wilfred.muema@gmail.com>
2020-07-25 18:25:52 +0300

    add the database uri link

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://willy:willy@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'secret'
@@ -15,7 +16,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://willy:willy@localhost/blog'
+    
     DEBUG = True
 
 config_options = {
