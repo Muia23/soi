d1474438e5edea4698867339d4b9758124768cfb
Muia23 <wilfred.muema@gmail.com>
2020-07-25 18:21:21 +0300

    add route function to render registration form page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
-from .forms import LoginForm
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user,logout_user,login_required
 from . import auth
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
@@ -15,3 +18,15 @@ def login():
     
     title = "Soi login"
     return render_template('auth/login.html', login_form = login_form, title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect(url_for('auth.login'))
+        title = "New Account"        
+    return render_template('auth/register.html',registration_form = form)
