3d0c7e64668d2c48c0e330206cad9b98b2956ac1
Muia23 <wilfred.muema@gmail.com>
2020-07-26 09:50:53 +0300

    add the request configuration

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,8 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/auth')
 
-    
+    from.requests import configure_request
+    configure_request(app)
     
 
     return app
