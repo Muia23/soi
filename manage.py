ad8949097a3176bfdf8bf5a2559d9acc84ca6702
Muia23 <wilfred.muema@gmail.com>
2020-07-26 09:56:57 +0300

    import the quote class

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Shell,Server
-from app.models import User
+from app.models import User, Blog, Quote
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
@@ -14,7 +14,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app ,db = db ,User = User)
+    return dict(app = app ,db = db ,User = User, Blog = Blog ,Quote = Quote,)
 
 if __name__ == '__main__':
     manager.run()
