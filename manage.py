88be862debf3225b96f78dfde7820a0d10e9aadb
Muia23 <wilfred.muema@gmail.com>
2020-07-27 14:02:53 +0300

    Import te Subscriber class

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Shell,Server
-from app.models import User, Blog, Quote, Comment
+from app.models import User, Blog, Quote, Comment, Subscriber
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
@@ -14,7 +14,7 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app ,db = db ,User = User, Blog = Blog ,Quote = Quote, Comment = Comment)
+    return dict(app = app ,db = db ,User = User, Blog = Blog ,Quote = Quote, Comment = Comment, Subscriber = Subscriber)
 
 if __name__ == '__main__':
     manager.run()
