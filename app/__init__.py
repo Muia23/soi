c021983227a541eef2657b77661afab5c4857456
Muia23 <wilfred.muema@gmail.com>
2020-07-27 17:47:55 +0300

    import flask-reuploaded

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+#from flask_reuploaded import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -11,10 +12,15 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+#photos = UploadSet('photos',IMAGES)
+
 def create_app(config_name):
     
     #Initialize the app
-    app = Flask(__name__)    
+    app = Flask(__name__)
+
+    #config UploadSet
+    #configure_uploads(app,photos)
 
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
